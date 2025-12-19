a21215317d121bf48a86a93aedb8a933d5a51ca7
saeed pathan <pathansaeed75@gmail.com>
2025-12-19 16:38:37 +0530

    code

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello from GCP CI/CD Pipeline 🚀"
 
-app.run(host="0.0.0.0", port=8080)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
